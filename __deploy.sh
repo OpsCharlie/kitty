0fe835b41879eba0f460759886fa9b14a989d074
OpsCharlie <verstraete.carl@gmail.com>
2025-09-28 21:05:47 +0200

    add deploy

---
+++ __deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+KITTY_DIR="/home/$USER/.local/kitty"
+BIN_DIR="/home/$USER/bin"
+
+[[ -d $BIN_DIR ]] || mkdir -p  $BIN_DIR
+[[ -d $KITTY_DIR ]] || mkdir -p "$KITTY_DIR"
+
+TEMP=$(command mktemp -d "/tmp/kitty-install-XXXXXXXXXXXX")
+mkdir -p "$TEMP/mp"
+
+case "$(command uname -m)" in
+    amd64 | x86_64) ARCH="x86_64" ;;
+    aarch64*) ARCH="arm64" ;;
+    armv8*) ARCH="arm64" ;;
+    *) echo "kitty binaries not available for architecture"; exit 1 ;;
+esac
+
+VERSION=$(curl -s https://api.github.com/repos/kovidgoyal/kitty/releases/latest | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+TAR="kitty-${VERSION/v/}-$ARCH.txz"
+URL="https://github.com/kovidgoyal/kitty/releases/download/$VERSION/$TAR"
+if ! curl -s "$URL" -L -o "$TEMP/$TAR"; then
+    echo "Download failed"
+    exit 1
+fi
+tar -xJof "$TEMP/$TAR" -C "$TEMP/mp" 
+cp -a "$TEMP"/mp/* "$KITTY_DIR"
+
+ln -sf "$KITTY_DIR/bin/kitty" "$KITTY_DIR/bin/kitten" "$BIN_DIR"
+
+cp "$KITTY_DIR"/share/applications/kitty.desktop ~/.local/share/applications/
+cp "$KITTY_DIR"/share/applications/kitty-open.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=$KITTY_DIR/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$BIN_DIR/kitty|g" ~/.local/share/applications/kitty*.desktop
+
+rm -rf "$TEMP"
+exit 0
